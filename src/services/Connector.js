f5c575d3a897ac9d3cd55813a61622b25e22837a
IvanBlacky <tcherivan@gmail.com>
2019-11-25 18:20:39 +0300

    Refactor: small fix

--- src/services/Connector.js
+++ src/services/Connector.js
@@ -59,7 +59,7 @@ class Connector extends BasicConnector {
                                 default: 'web',
                             },
                             referralId: {
-                                type: 'string'
+                                type: 'string',
                             },
                             testingPass: {
                                 type: 'string',
